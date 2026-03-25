b5c84b64e0fb83a713c4194d3acae71f87bebd12
chido <chidodesigns@gmail.com>
2026-03-25 14:42:24 +0000

    implement next iamge in main header

--- components/header/main-header.js
+++ components/header/main-header.js
@@ -1,12 +1,20 @@
 import Link from "next/link";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
+import Image from "next/image";
 
 export default function MainHeader() {
   return (
     <header className={classes.header}>
       <Link href="/" className={classes.logo}>
-        <img src={logoImg.src} alt="A plate with food on it" />
+        <Image
+          src={logoImg}
+          /**loading lazy vs eager - what is the difference? The difference between lazy and eager loading is when the image is loaded. Lazy loading defers loading the image until it is about to enter the viewport, which can improve page load performance. Eager loading loads the image immediately, which can be useful for above-the-fold content. */
+          loading="eager"
+          alt="A plate with food on it"
+          width={80}
+          height={80}
+        />
         NextLevel Food
       </Link>
       <nav className={classes.nav}>
