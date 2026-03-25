3e324e322f5546b475f46f4ccade3da2486464ae
chido <chidodesigns@gmail.com>
2026-03-25 14:49:27 +0000

    cmp dir clean up

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import "./globals.css";
-import MainHeader from "@/components/header/main-header";
+import MainHeader from "@/components/main-header/header/main-header";
 
 export const metadata = {
   title: "NextLevel Food",
