4609c3b74a55b1d50750291533094f7c3987608f
chido <chidodesigns@gmail.com>
2026-03-25 14:47:25 +0000

    created and added main-header-background cmp

--- components/header/main-header.js
+++ components/header/main-header.js
@@ -2,31 +2,35 @@ import Link from "next/link";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import Image from "next/image";
+import MainHeaderBackground from "../main-header-background/main-header-background";
 
 export default function MainHeader() {
   return (
-    <header className={classes.header}>
-      <Link href="/" className={classes.logo}>
-        <Image
-          src={logoImg}
-          /**loading lazy vs eager - what is the difference? The difference between lazy and eager loading is when the image is loaded. Lazy loading defers loading the image until it is about to enter the viewport, which can improve page load performance. Eager loading loads the image immediately, which can be useful for above-the-fold content. */
-          loading="eager"
-          alt="A plate with food on it"
-          width={80}
-          height={80}
-        />
-        NextLevel Food
-      </Link>
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <Link href="/meals">Browse Meals</Link>
-          </li>
-          <li>
-            <Link href="/community">Foodies Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link href="/" className={classes.logo}>
+          <Image
+            src={logoImg}
+            /**loading lazy vs eager - what is the difference? The difference between lazy and eager loading is when the image is loaded. Lazy loading defers loading the image until it is about to enter the viewport, which can improve page load performance. Eager loading loads the image immediately, which can be useful for above-the-fold content. */
+            loading="eager"
+            alt="A plate with food on it"
+            width={80}
+            height={80}
+          />
+          NextLevel Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
