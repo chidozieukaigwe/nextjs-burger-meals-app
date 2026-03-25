ba5eeca2f5092f9044e7a91386807e9c9c864bfe
chido <chidodesigns@gmail.com>
2026-03-25 21:20:18 +0000

    not found page added for meals

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,10 +1,15 @@
 import Image from "next/image";
 import classes from "./page.module.css";
 import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
 export default async function MealDetailsPage({ params }) {
   const { mealSlug } = await params;
   const meal = getMeal(mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
 
   /**
    * The instructions for the meal are stored as plain text with newline characters to indicate line breaks. However, when rendering this text in HTML, the newline characters will not be interpreted as line breaks. To ensure that the instructions are displayed correctly with line breaks, we need to replace the newline characters with HTML line break tags (<br />). This way, when the instructions are rendered in the browser, they will appear with the intended formatting, making it easier for users to read and follow the recipe steps.
