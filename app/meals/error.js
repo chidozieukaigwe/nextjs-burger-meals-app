65a28417895e849f21b6adb9eae2a64d186c0f65
chido <chidodesigns@gmail.com>
2026-03-25 16:26:14 +0000

    add error page for meals path

---
+++ app/meals/error.js
@@ -0,0 +1,9 @@
+"use client";
+export default function Error() {
+  return (
+    <main className="error">
+      <h1>Something went wrong!</h1>
+      <p>We couldn't fetch the meals. Please try again later.</p>
+    </main>
+  );
+}
