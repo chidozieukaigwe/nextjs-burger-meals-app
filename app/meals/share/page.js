80010092bacd1d9ed3d04bb8b675cf504a067785
chido <chidodesigns@gmail.com>
2026-03-25 23:47:33 +0000

    useActionState to show form error state from a server action and preserve state of form upon errors

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,9 +1,18 @@
+"use client";
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
 import { shareMeal } from "@/lib/actions";
 import MealsFormSubmit from "@/components/meals/meals-form-submit";
+import { useActionState } from "react";
 
 export default function ShareMealPage() {
+  /**
+   * useActionState is a custom hook that allows us to track the state of the server action (shareMeal) when the form is submitted. It provides information about whether the action is currently being executed, if it has completed successfully, or if it has encountered an error. This can be used to provide feedback to the user, such as showing a loading indicator while the meal is being shared or displaying a success message once the meal has been shared successfully. By using this hook, we can enhance the user experience by providing real-time feedback on the status of their meal sharing action.
+   */
+  const [state, formAction] = useActionState(shareMeal, {
+    message: null,
+  });
+
   return (
     <>
       <header className={classes.header}>
@@ -13,7 +22,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -42,6 +51,7 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="Pick an image for your meal" name="image" />
+          {state.message && <p className={classes.error}>{state.message}</p>}
           <p className={classes.actions}>
             <MealsFormSubmit />
           </p>
